ee7518a0bcc1d3379c590835dfc4310b493de0b8
Sean Jung <sean.jung23@gmail.com>
2023-04-13 20:07:28 -0400

    Completed App.jsx until level 4. Did not get API running yet but website is functional

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 var movies = [
-  {title: 'Mean Girls'},
-  {title: 'Hackers'},
-  {title: 'The Grey'},
-  {title: 'Sunshine'},
-  {title: 'Ex Machina'}
+  {title: 'Mean Girls', watched: false},
+  {title: 'Hackers', watched: false},
+  {title: 'The Grey', watched: false},
+  {title: 'Sunshine', watched: false},
+  {title: 'Ex Machina', watched: false}
 ];
 
 /*----------------------Movie App----------------------*/
@@ -25,101 +25,123 @@ const App = () => {
           <AddMovie movieDataSetter={setMovieData}/>
           <Search movieDataSetter={setMovieData}/>
           <WatchedAlready movieDataSetter={setMovieData}/>
-          <ToWatch/>
-          <MovieList movies={movieData}/>
+          <ToWatch movieDataSetter={setMovieData}/>
+          <MovieList movieList={movieData}/>
       </div>
     </div>
   )
 };
 
 
-/*----------------------Movie Watched----------------------*/
-const WatchedAlready = ({movieDataSetter}) => (
-  <div>
-    <button onClick={alert('hi')}>Watched Already</button>
-  </div>
-);
+/*----------------------Movies Watched----------------------*/
+const WatchedAlready = ({movieDataSetter}) => {
+  var showWatched = () => {
+    var newData = [];
 
-var showWatched = (cb) => {
-  var tags = document.getElementsByTagName("li");
-  var newData = [];
-
-  for (var j = 0; j < tags.length; j++) {
-    if (tags[j].attributes.watched.value) {
-      for (var n = 0; n < movies.length; n++) {
-        if (movies[n].title === tags[j].innerText) {
-          newData.push(movies[n]);
-        }
+    for (var j = 0; j < movies.length; j++) {
+      if (movies[j].watched) {
+        newData.push(movies[j]);
       }
     }
-  }
 
-  if (newData.length === 0) {
-    newData.push({title:'no watched movie yet :('});
-  }
+    if (newData.length === 0) {
+      newData.push({title:'no watched movie yet :('});
+    }
 
-  cb(newData);
+    movieDataSetter(newData);
+  };
+
+  return (
+    <div>
+      <button onClick={() => showWatched()}>Watched Already</button>
+    </div>
+  );
 };
 
 
-/*----------------------Movie Watched----------------------*/
-const ToWatch = () => (
-  <button onClick={showToWatch}>To Watch</button>
-);
+/*----------------------Movies To Watch----------------------*/
+const ToWatch = ({movieDataSetter}) => {
+  var showToWatch = () => {
+    var newData = [];
 
-var showToWatch = () => {
+    for (var j = 0; j < movies.length; j++) {
+      if (!movies[j].watched) {
+        newData.push(movies[j]);
+      }
+    }
+
+    if (newData.length === 0) {
+      newData.push({title:'no movie to watch yet :('});
+    }
+    console.log(newData);
+
+    movieDataSetter(newData);
+  };
+
+  return (
+    <div>
+      <button onClick={() => showToWatch()}>To Watch</button>
+    </div>
+  );
 };
 
 
-/*----------------------Movie Add----------------------*/
-const AddMovie = ({movieDataSetter}) => (
-  <div>
-    <input className='user-input' type='text' placeholder='Add movie title here'/>
-    <button onClick={() => addToMovieList(movieDataSetter)}>Add</button>
-  </div>
-);
+/*----------------------Add Movie----------------------*/
+const AddMovie = ({movieDataSetter}) => {
+  var addToMovieList = () => {
+    var newMovie = document.getElementsByClassName('user-input')[0].value;
 
-var addToMovieList = () => {
-  var newMovie = document.getElementsByClassName('user-input')[0].value;
+    if (newMovie.length !== 0) {
+      movies.push({title: newMovie, watched: false});
+    }
 
-  if (newMovie.length !== 0) {
-    movies.push({title: newMovie})
-  }
+    movieDataSetter(movies);
+  };
+
+  return (
+    <div>
+      <input className='user-input' type='text' placeholder='Add movie title here'/>
+      <button onClick={() => addToMovieList()}>Add Movie</button>
+    </div>
+  );
 };
 
 
-/*----------------------Movie Search----------------------*/
+/*----------------------Search Movie----------------------*/
 const Search = ({movieDataSetter}) => {
+  var searcher = () => {
+    var query = (document.getElementsByClassName('search-query')[0].value).toLowerCase();
+    var newData = [];
+
+    for (var i = 0; i < movies.length; i++) {
+      if (movies[i].title.toLowerCase().indexOf(query) !== -1) {
+        newData.push(movies[i]);
+      }
+    }
+    if (newData.length === 0) {
+      newData.push({title:'no movie by that name found :\'('});
+    }
+
+    movieDataSetter(newData);
+  };
+
   return (
     <div>
       <input className='search-query' type='text' placeholder='Search...' onChange={() => searcher(movieDataSetter)}/>
       <button onClick={() => searcher(movieDataSetter)}>Go!</button>
+      <div>
+        <button onClick={() => searcher(movieDataSetter)}>My List</button>
+      </div>
     </div>
   )
 };
 
-var searcher = (cb) => {
-  var query = (document.getElementsByClassName('search-query')[0].value).toLowerCase();
-  var newData = [];
-
-  for (var i = 0; i < movies.length; i++) {
-    if (movies[i].title.toLowerCase().indexOf(query) !== -1) {
-      newData.push(movies[i]);
-    }
-  }
-  if (newData.length === 0) {
-    newData.push({title:'no movie by that name found'});
-  }
-
-  cb(newData);
-};
-
 
 /*----------------------Movie List----------------------*/
-const MovieList = ({movies}) => (
+const MovieList = ({movieList}) => (
   <div>
     <ul>
-      {movies.map((movie, index) => (
+      {movieList.map((movie, index) => (
         <MovieListEntry key={index} movie={movie}/>
       ))}
     </ul>
@@ -127,19 +149,37 @@ const MovieList = ({movies}) => (
 );
 
 const MovieListEntry = ({movie}) => {
-  const [isWatched, setIsWatched] = React.useState(false);
+  const [isWatched, setIsWatched] = React.useState(movie.watched);
+  const [isShown, setIsShown] = React.useState(false);
+
   const style = {
-    color: isWatched ? '#00FF00' : '#FF0000'
+    color: movie.watched/*isWatched*/ ? '#00FF00' : '#FF0000'
+  }
+
+  var watchedSetter = () => {
+    movie.watched = !isWatched;
+    setIsWatched(movie.watched);
+  };
+
+  var showDetails = () => {
+    setIsShown(!isShown);
   }
 
   return (
     <div>
-      <li watched={'' + isWatched}>{movie.title}</li>
-      <div>
-        <button style={style} onClick={() => {setIsWatched(!isWatched)}}>Watched</button>
-      </div>
+      <li onClick={() => showDetails()}>{movie.title}</li>
+      {isShown && (
+        <div>
+          <p><strong>Year:</strong></p>
+          <p><strong>Runtime:</strong></p>
+          <p><strong>Metascore:</strong></p>
+          <p><strong>imdbRating:</strong></p>
+          <button style={style} onClick={() => {watchedSetter()}}>Watched</button>
+        </div>
+      )}
     </div>
   )
 };
+
 
 export default App;
